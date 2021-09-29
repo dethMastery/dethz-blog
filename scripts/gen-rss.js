178a840363332b9a9a847331a4a6bb2a2c2245b1
Detzz <a2z.team.198@gmail.com>
2021-09-29 08:38:44 +0700

    edit rss gen

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: "Deth's Blog",
+    site_url: 'https://blog.detzz.in.th',
+    feed_url: 'https://blog.detzz.in.th/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
