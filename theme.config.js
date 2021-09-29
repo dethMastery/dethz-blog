1185ab13769f4ec160d34a3304456358c3a40730
Detzz <a2z.team.198@gmail.com>
2021-09-29 07:37:54 +0700

    updated style

--- theme.config.js
+++ theme.config.js
@@ -2,7 +2,7 @@ const YEAR = new Date().getFullYear()
 
 export default {
   footer: (
-    <small style={{ display: 'block', marginTop: '8rem' }}>
+    <small style={{ display: 'block', marginTop: '8rem', color: '#EDEDED' }}>
       © <time>{YEAR}</time> Suphakit P. All Right Reserved
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
