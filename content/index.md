843c8971366cf3175dcef1750845de279de9dbfd
dethMastery <owner@detzz.in.th>
2021-12-14 03:19:33 +0000

    adding index page

--- content/index.md
+++ content/index.md
@@ -1,12 +1,12 @@
 ---
 type: page
-title: Welcome To Deth's Blog!
+title: Index
+hTitle: Welcome to Deth's Blog!!
 thumbnail: "https://media.discordapp.net/attachments/832221566820548667/892587613402320906/New_Project.png"
 date: 2021-09-29
 ---
-
   ## **สวัสดีครับทุกคน** 
-  &nbsp;&nbsp;&nbsp; ยินดีต้อนรับเข้าสู่ Blog เล็กๆ แห่งนี้ ซึ่งสำหรับคนที่เคยเข้ามาก่อนแล้วอาจจะรู้สึกแปลกๆ นิดหน่อย เพราะว่าใช่ครับ นี่คือการเปลี่ยน FrameWork จาก Next.js ไปเป็น Nuxt.js ครับผม เนื่องด้วยตตัวผมเองรู้สึกว่าของเก่านั้นยังไม่ดีพอที่จะเอาไปใช้งานได้จริงๆ นั้นแหละครับ เลยทำใหม่ขึ้นมาเลย สำหรับคนที่อยากจะติดตามนะครับ เชิญที่ Link ข้างล่างได้เลยครับ
+  &nbsp;&nbsp;&nbsp; ยินดีต้อนรับเข้าสู่ Blog เล็กๆ แห่งนี้ ซึ่งสำหรับคนที่เคยเข้ามาก่อนแล้วอาจจะรู้สึกแปลกๆ นิดหน่อย เพราะว่าใช่ครับ นี่คือการเปลี่ยน FrameWork จาก Next.js ไปเป็น Nuxt.js ครับผม เนื่องด้วยตัวผมเองรู้สึกว่าของเก่านั้นยังไม่ดีพอที่จะเอาไปใช้งานได้จริงๆ นั้นแหละครับ เลยทำใหม่ขึ้นมาเลย ยิ่งด้วยล่าสุดเลยคือ vercel ทำพังไปแล้วด้วย ;-; <br><br> สำหรับคนที่อยากจะติดตามนะครับ เชิญที่ Link ข้างล่างได้เลยครับ
 
 ---
 
@@ -14,7 +14,7 @@ date: 2021-09-29
   
   [![HomePage](https://img.shields.io/badge/-HomePage-black?style=for-the-badge)](https://detzz.in.th)
 
-  [![FB](https://img.shields.io/badge/Facebook-Geroge%20Deterk-1978F3?style=for-the-badge)](https://fb.com/detzz.in.th)
+  [![FB](https://img.shields.io/badge/Facebook-Geroge%20Katsuragi%20Deterk-1978F3?style=for-the-badge)](https://fb.com/detzz.in.th)
 
   [![Twitter](https://img.shields.io/badge/Twitter-@JojiDetzz-skyblue?&style=for-the-badge)](https://twitter.com/JojiDetzz)
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,59 @@
 <template>
-  <Tutorial/>
+  <div>
+      <h1>
+          {{ index.hTitle }}
+      </h1>
+
+      <content>
+        <nuxt-content :document="index" />
+      </content>
+  </div>
 </template>
 
 <script>
-export default {}
+    export default {
+        async asyncData ({ $content, params }) {
+            const index = await $content('/index').fetch()
+            return {
+                index
+            }
+        },
+        head() {
+            return {
+                title: this.index.title + " | Deth's Blog",
+                meta: [
+                    {
+                        hid: 'og:title',
+                        name: 'og:title',
+                        content: this.index.title + " | Deth's Blog"
+                    },
+                    {
+                        hid: 'og:site_name',
+                        name: 'og:site_name',
+                        content: this.index.title + " | Deth's Blog"
+                    },
+                    {
+                        hid: 'og:type',
+                        name: 'og:type',
+                        content: 'article'
+                    },
+                    {
+                        hid: 'og:image',
+                        name: 'og:image',
+                        content: this.index.thumbnail
+                    },
+                    {
+                        hid: 'description',
+                        name: 'description',
+                        content: this.index.description
+                    },
+                    {
+                        hid: 'author',
+                        name: 'author',
+                        content: 'Suphakit P.'
+                    },
+                ]
+            }
+        }
+    }
 </script>
