d1fb842c925295f86c5811ef491228c06cb6fadc
dethMastery <owner@detzz.in.th>
2021-12-14 09:38:45 +0000

    add posts page

---
+++ pages/posts/_slug.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="container-fluid">
+    <main :style="'background: url('+post.image+')'" class="header-bg">
+    </main>
+    <center>
+      <main class="content">
+        <ul>
+          <li class="pos">
+            <a href="/posts">
+              <span> &lt; Back </span>
+            </a>
+          </li>
+        </ul>
+        
+        <h1 class="title">{{ post.title }}</h1>
+        <span class="date">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Publish Date: {{ formatDate(post.updatedAt) }}</span>
+        
+        <nuxt-content :document="post" />
+      </main>
+    </center>
+  </div>
+</template>
+
+<script>
+  export default {
+
+      async asyncData({ $content, params }) {
+        data: {
+          const posts = await $content('posts')
+            .only(['title', 'slug'])
+            .sortBy('createdAt', 'asc')
+            .fetch()
+
+          const post = await $content('posts', params.slug).fetch()
+
+          return {
+            posts,
+            post
+          }
+        }
+      },
+    methods: {
+      formatDate(date) {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' }
+        return new Date(date).toLocaleDateString('en', options)
+      }
+    },
+    head() {
+        return {
+            title: this.post.title + " | Deth's Blog",
+            meta: [
+                {
+                    hid: 'og:title',
+                    name: 'og:title',
+                    content: this.post.title + " | Deth's Blog"
+                },
+                {
+                    hid: 'og:site_name',
+                    name: 'og:site_name',
+                    content: this.post.title + " | Deth's Blog"
+                },
+                {
+                    hid: 'og:type',
+                    name: 'og:type',
+                    content: 'article'
+                },
+                {
+                    hid: 'og:image',
+                    name: 'og:image',
+                    content: this.post.thumbnail
+                },
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: this.post.description
+                },
+                {
+                    hid: 'author',
+                    name: 'author',
+                    content: 'Suphakit P.'
+                },
+            ]
+        }
+    }
+  }
+</script>
+
+<style>
+  @import url(../style/based.css);
+</style>
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,3 +1,78 @@
 <template>
-    
+    <div>
+        <h1>
+            Posts
+        </h1>
+        <Nav />
+        <ul class="listing">
+            <li v-for="article of articles" :key="article.slug">
+                <div class="hover" :style="'background: url(' + article.thumbnail +');'">
+                    <div class="black">
+                        <NuxtLink :to="{ name: 'posts-slug', params: { slug: article.slug } }">
+                            <h2 class="titletext">{{ article.title }}</h2>
+                        </NuxtLink>
+                        <span class="desptext"><i>{{ article.description }}</i></span><br>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
 </template>
+
+<style>
+@import url(../style/based.css);
+@import url(../style/Nav.css);
+@import url(../style/post.css);
+</style>
+
+<script>
+  export default {
+    async asyncData({ $content, params }) {
+      const articles = await $content('posts', params.slug)
+        .only(['title', 'description', 'thumbnail', 'slug'])
+        .sortBy('createdAt', 'desc')
+        .fetch()
+
+      return {
+        articles
+      }
+    },
+    head() {
+        return {
+            title: "Posts | Deth's Blog",
+            meta: [
+                {
+                    hid: 'og:title',
+                    name: 'og:title',
+                    content: "Posts | Deth's Blog"
+                },
+                {
+                    hid: 'og:site_name',
+                    name: 'og:site_name',
+                    content: "Posts | Deth's Blog"
+                },
+                {
+                    hid: 'og:type',
+                    name: 'og:type',
+                    content: 'article'
+                },
+                {
+                    hid: 'og:image',
+                    name: 'og:image',
+                    content: ''
+                },
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: ''
+                },
+                {
+                    hid: 'author',
+                    name: 'author',
+                    content: 'Suphakit P.'
+                },
+            ]
+        }
+    }
+  }
+</script>
