6cd813e77b017ec60505c2722bc5ba8956e2aa4f
dethMastery <owner@detzz.in.th>
2022-01-13 02:58:22 +0700

    added error page

---
+++ layouts/error.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <div class="gradient-border" id="box">
+      <center>
+        Sorry, But <br />
+        There're <br />
+        Nothing Here
+      </center>
+    </div>
+    <br>
+    &lt; <a @click="$router.go(-1)" class="b-link">Go back</a>
+  </div>
+</template>
+
+<style>
+  @import url(./error.css);
+</style>
