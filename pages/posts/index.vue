61143b549cb910df1853cebbf9dd39801c60adcd
dethMastery <owner@detzz.in.th>
2022-03-21 22:26:10 +0700

    updated: solution

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -7,7 +7,8 @@
         <br>
         <ul class="listing">
             <li v-for="article of articles" :key="article.slug">
-                <div class="hover" :style="'background: url(' + article.thumbnail +');'">
+                <div style="display: none;" v-if="article.title == undefined"></div>
+                <div class="hover" :style="'background: url(' + article.thumbnail +');'" v-else>
                     <div class="black blacko">
                         <NuxtLink :to="{ name: 'posts-slug', params: { slug: article.slug } }">
                             <h2 class="titletext">{{ article.title }}</h2>
